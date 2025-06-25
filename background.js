07525f9b1fe8470f3a5c44c1b8826e246dfef991
prasad <hackerspider09@gmail.com>
2025-06-25 15:43:31 +0530

    just comments

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 console.log('Hello from Prash');
+// Developed By Prasad (hackerspider09)
+
 
 chrome.runtime.onInstalled.addListener(function(details) {
   // console.log('Extension installed or browser started');
@@ -6,7 +8,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
   // Automatically trigger assignPreValues when the extension is installed or browser is started
   if (details.reason === 'install' || details.reason === 'update') {
     chrome.management.setEnabled(chrome.runtime.id, true, function() {
-      // console.log('Extension enabled after installation/update');
+      console.log('Extension enabled after installation/update');
     });
   }
 });
@@ -16,6 +18,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // Check if the tab has completed loading
     if (changeInfo.status === 'complete') {
       // Trigger the action when the tab is fully loaded
+      console.log("Sending assignPreValues after refresh.");
       chrome.tabs.sendMessage(tabId, { action: 'assignPreValues' });
     }
   });
